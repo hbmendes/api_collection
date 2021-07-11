c5e0c63e0ac87a071e37baa9ffd621e3703ac7c0
Harielson <harielson.mendes@gmail.com>
2021-07-11 15:02:07 -0300

    Criado testes com Int Array

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteInArray.kt
@@ -0,0 +1,30 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val values = IntArray(5)
+    values[0] = 1
+    values[1] = 2
+    values[2] = 3
+    values[3] = 4
+    values[4] = 5
+
+    for (valor in values){
+        println(valor)
+    }
+
+    println("########################")
+    values.forEach { valor: Int ->
+        println(valor)
+    }
+
+    println("########################")
+    for (index in values.indices){
+        println(values[index])
+    }
+
+    println("########################")
+    values.sort()
+    for (valor in values){
+        println(valor)
+    }
+}
