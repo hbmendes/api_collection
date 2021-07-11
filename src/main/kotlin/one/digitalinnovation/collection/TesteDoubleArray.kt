1824e1a8f56eb74f32fb151b0cf3796572179b61
Harielson <harielson.mendes@gmail.com>
2021-07-11 16:05:08 -0300

    Criado testes com Double Array

--- src/main/kotlin/one/digitalinnovation/collection/TesteDoubleArray.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteDoubleArray.kt
@@ -6,16 +6,33 @@ fun main(){
     salarios[1] = 3000.0
     salarios[2] = 500.0
 
+    var MENOR_VALOR:Double = 99999999.0
+    var MAIOR_VALOR:Double = 0.0
+
     salarios.forEach { println(it) }
 
-    println("################")
-    salarios.forEachIndexed { index, salario ->
-        salarios[index] = salario * 1.1
+    println("###FUNCAO###")
+    salarios.forEachIndexed { i, x ->
+        if (salarios[i] <= MENOR_VALOR){
+            MENOR_VALOR = x
+        }
+        if (salarios[i] >= MAIOR_VALOR){
+            MAIOR_VALOR = x
+        }
     }
-    salarios.forEach { println(it) }
+    println("###MENOR_VALOR###")
+    println(MENOR_VALOR)
+    println("###MAIOR_VALOR###")
+    println(MAIOR_VALOR)
 
     println("################")
     val salarios2 = doubleArrayOf(1500.0, 1250.0, 5000.0)
     salarios2.sort()
     salarios2.forEach { println(it) }
+
+    println("##CALCULO###")
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
 }
